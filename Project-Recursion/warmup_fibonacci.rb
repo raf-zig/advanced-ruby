e9d720cd85b7ac882758e56c904367bc310ba7fb
raf-zig <ziravin@yandex.ru>
2023-03-03 12:28:46 +0500

    Add a recursive method

--- Project-Recursion/warmup_fibonacci.rb
+++ Project-Recursion/warmup_fibonacci.rb
@@ -1,4 +1,4 @@
-def fibs (n)
+def fibs(n)
   arr = [0]
   if n == 1
      arr << 1
@@ -12,3 +12,13 @@ def fibs (n)
   end
   arr
 end
+
+def fibs_rec(n)
+  return [0] if n == 0
+  return [0, 1] if n == 1
+  arr = fibs_rec(n - 1)
+  arr << arr[-2] + arr[-1]
+end
+
+p fibs (5)
+p fibs_rec(5)
